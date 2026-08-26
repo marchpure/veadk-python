859b0426237060f810ead669947a2f58cb44c3aa
haoxingjun <haoxingjun@bytedance.com>
2026-08-26 14:01:37 +0800

    build: refresh knowledge workspace production assets

---
+++ veadk/webui/assets/visualizations/mermaid/swimlanesDiagram-ULZ7WXOC-b1DupC1A.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-UKHOOZJN-BICe-9bY.js";import{a}from"./mermaid.core-qXp3Jyzv.js";import"../../chunks/studio-entry-pz-m2g3t.js";import"./chunk-5VM5RSS4-xqNQCqxb.js";import"./chunk-XXDRQBXY-BS5lg0Na.js";import"./chunk-KBJHAD2P-BliIgWy0.js";import"./chunk-2GRJ4B5K-DMtvP7ji.js";import"../../chunks/channel-D9kfdvyj.js";import"../../app/index-Dp2aFcoU.js";import"../../chunks/index-BqlOLskA.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,g=r({defaultLayout:"swimlane",styles:m});export{g as diagram};
