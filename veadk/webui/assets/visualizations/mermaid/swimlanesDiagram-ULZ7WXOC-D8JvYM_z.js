d09b163e3c2cfd29a48f039a2bb48efee0d1b968
haoxingjun <haoxingjun@bytedance.com>
2026-08-26 16:12:36 +0800

    build: refresh production webui assets

--- veadk/webui/assets/visualizations/mermaid/swimlanesDiagram-ULZ7WXOC-D8JvYM_z.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-UKHOOZJN-rGjJ78ot.js";import{a}from"./mermaid.core-DuUzA35B.js";import"../../chunks/studio-entry-DMNAmFaS.js";import"./chunk-5VM5RSS4-DVk2G70h.js";import"./chunk-XXDRQBXY-DbBDdXyV.js";import"./chunk-KBJHAD2P-CrPzyrfz.js";import"./chunk-2GRJ4B5K-CzTnJFj_.js";import"../../chunks/channel-BAH5SN4O.js";import"../../app/index-DOZysVM7.js";import"../../chunks/index-BqlOLskA.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,g=r({defaultLayout:"swimlane",styles:m});export{g as diagram};
